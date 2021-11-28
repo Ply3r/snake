ea24024e602be663e6f497145e6447a5becf1ceb
Leandro Henrique <Play3rTox1c@gmail.com>
2021-11-28 14:49:13 -0300

    add game_over e restart e cor na snake

--- snake.py
+++ snake.py
@@ -8,7 +8,6 @@ GREEN = [0, 255, 0]
 WHITE = [255, 255, 255]
 RED = [255, 0, 0]
 FPS = 30
-VELOCITY = 20
 
 pygame.font.init()
 scoreText = pygame.font.SysFont('Comic Sans MS', 30)
@@ -16,7 +15,7 @@ scoreText = pygame.font.SysFont('Comic Sans MS', 30)
 WINDOW = pygame.display.set_mode([WIDTH, HEIGHT])
 pygame.display.set_caption("Snake")
 
-SNAKE = [[200, 200], [220, 200]]
+snake = [[200, 200], [220, 200]]
 
 APPLE = pygame.Surface((20, 20))
 APPLE.fill(RED)
@@ -32,7 +31,7 @@ def generate_position():
     x = randint(0, WIDTH - 40)
     y = randint(0, HEIGHT - 40)
     position = [x, y]
-    for snake_pos in SNAKE:
+    for snake_pos in snake:
         if (snake_pos[0] - 20 <= position[0] <= snake_pos[0] + 20) \
                 and (snake_pos[1] - 20 <= position[1] <= snake_pos[1] + 20):
             generate_position()
@@ -52,58 +51,109 @@ def change_direction(direction):
     return direction
 
 
-def move_snake(direction):
+def move_snake(direction, velocity):
     if direction == 'RIGHT':
-        SNAKE.insert(0, [SNAKE[0][0] + VELOCITY, SNAKE[0][1]])
+        snake.insert(0, [snake[0][0] + velocity, snake[0][1]])
     if direction == 'LEFT':
-        SNAKE.insert(0, [SNAKE[0][0] - VELOCITY, SNAKE[0][1]])
+        snake.insert(0, [snake[0][0] - velocity, snake[0][1]])
     if direction == 'UP':
-        SNAKE.insert(0, [SNAKE[0][0], SNAKE[0][1] - VELOCITY])
+        snake.insert(0, [snake[0][0], snake[0][1] - velocity])
     if direction == 'DOWN':
-        SNAKE.insert(0, [SNAKE[0][0], SNAKE[0][1] + VELOCITY])
-    SNAKE.pop()
+        snake.insert(0, [snake[0][0], snake[0][1] + velocity])
+    snake.pop()
 
 
 def random_rgb(size):
-    first_color = Color('#fc00ff')
-    last_colot = Color('#00dbde')
+    first_color = Color('#12c2e9')
+    last_color = Color('#f64f59')
+    arrayOfColors = list(first_color.range_to(last_color, size))
+    hold = []
+    for color in arrayOfColors:
+        r = ((color.get_red() * 255) * 10) // 10
+        g = ((color.get_green() * 255) * 10) // 10
+        b = ((color.get_blue() * 255) * 10) // 10
+        hold.append([r, g, b])
+    return hold
 
 
 def eat_apple(apple):
-    x = SNAKE[0][0]
-    y = SNAKE[0][1]
+    x = snake[0][0]
+    y = snake[0][1]
     if (x - 20 <= apple.x <= x + 20) and (y - 20 <= apple.y <= y + 20):
-        SNAKE.append([SNAKE[-1][0] + 20, SNAKE[-1][1]])
+        snake.append([snake[-1][0], snake[-1][1]])
         return True
     return False
 
 
-def draw_window(apple, score):
-    scoreRender = scoreText.render(f'Score {score}', False, WHITE)
+def draw_window(apple, score, game_over):
+    if game_over:
+        game_over_text = scoreText.render('Game Over', False, WHITE)
+        WINDOW.fill([0, 0, 0])
+        WINDOW.blit(game_over_text, [WIDTH//2 - 80, HEIGHT//2 - 40])
+    else:
+        scoreRender = scoreText.render(f'Score {score}', False, WHITE)
 
-    WINDOW.fill([0, 0, 0])
-    WINDOW.blit(scoreRender, [0, 0])
-    WINDOW.blit(APPLE, [apple.x, apple.y])
+        WINDOW.fill([0, 0, 0])
+        WINDOW.blit(scoreRender, [0, 0])
+        WINDOW.blit(APPLE, [apple.x, apple.y])
 
-    colors = random_rgb(len(SNAKE))
+        colors = random_rgb(len(snake))
 
-    index = 0
-    for pos in SNAKE:
-        SNAKE_SKIN = pygame.Surface((20, 20))
-        SNAKE_SKIN.fill(colors[index])
+        index = 0
+        for pos in snake:
+            SNAKE_SKIN = pygame.Surface((20, 20))
+            SNAKE_SKIN.fill(colors[index])
 
-        WINDOW.blit(SNAKE_SKIN, pos)
-        index += 1
+            WINDOW.blit(SNAKE_SKIN, pos)
+            index += 1
 
+
+def check_body_colision():
+    global score
+    global game_over
+    snake_head = snake[0]
+    for index in range(len(snake)):
+        print(snake_head, snake[index])
+        if snake_head == snake[index] and index != 0:
+            score = 0
+            game_over = True
+
+
+def check_game_over(game_over):
+    snake_head = snake[0]
+    global score
+    if snake_head[0] <= -20 or snake_head[0] >= WIDTH + 20:
+        game_over = True
+        score = 0
+    if snake_head[1] <= -20 or snake_head[1] >= HEIGHT + 20:
+        game_over = True
+        score = 0
+    check_body_colision()
+    return game_over
+
+
+def restar_game(game_over):
+    key_pressed = pygame.key.get_pressed()
+    
+    if key_pressed[pygame.K_r] and game_over:
+        INITIAL_STATE = [[200, 200], [220, 200]]
+        global snake
+        snake = INITIAL_STATE
+        game_over = False
+
+    return game_over
+
+
+direction = 'RIGHT'
+score = 0
+velocity = 20
+game_over = False
 
 def game_start():
     clock = pygame.time.Clock()
 
     apple_position = generate_position()
     apple = pygame.Rect(*apple_position, 20, 20)
-
-    direction = 'RIGHT'
-    score = 0
 
     run = True
     while run:
@@ -112,8 +162,13 @@ def game_start():
             if event.type == pygame.QUIT:
                 run = False
 
+        global direction
+        global velocity
+        global game_over
+        global score
+
         direction = change_direction(direction)
-        move_snake(direction)
+        move_snake(direction, velocity)
 
         comeu = eat_apple(apple)
         if comeu:
@@ -122,7 +177,11 @@ def game_start():
             apple.y = apple_position[1]
             score += 1
 
-        draw_window(apple, score)
+        
+        game_over = check_game_over(game_over)
+        game_over = restar_game(game_over)
+
+        draw_window(apple, score, game_over)
         pygame.display.update()
 
 
